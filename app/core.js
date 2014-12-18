b2d5dc08eec5d7f8ada185f197edccb8336f7240
Stefan Röthlisberger <sfroeth@gmail.com>
2014-12-18 20:18:06 +0100

    Added LayoutLoader and WorkspaceConfigLoader.

--- app/core.js
+++ app/core.js
@@ -52,16 +52,18 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
 
     function WorkspaceConfig(name) {
         this.name = name;
-        this.configFileUrl = 'workspace-configs/' + name + '.json';
+        this.layout = {};
+        this.widgets = [];
     }
 
     function Layout(name) {
         this.name = name;
-        this.htmlFile = 'layouts/' + name + '.html';
+        this.document = {};
     }
 
     function Widget(name) {
         this.name = name;
+        this.viewTarget = null;
         this.document = {};
         this.elements = [];
     }
@@ -73,6 +75,7 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
     heir.inherit(Element, EventEmitter);
     heir.inherit(Widget, EventEmitter);
     heir.inherit(Layout, EventEmitter);
+    heir.inherit(WorkspaceConfig, EventEmitter);
 
 
     function Cache() {
@@ -177,6 +180,7 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
                 var element = ElementLoader.loading.get(url);
                 ElementLoader.cache.add(element);
                 ElementLoader.loading.remove(url);
+                log('Element ' + name + ' has been loaded');
                 onLoadedCallback(element);
 
                 // notify the listeners added while the element was loading
@@ -191,6 +195,7 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
         this.numberOfElementsLoaded = 0;
         this.indexHtmlLoaded = false;
         this.widget = null;
+        this.configUrl = null;
         this.onLoadedCallback = function () {
         };
     }
@@ -199,29 +204,27 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
 
     WidgetLoader.prototype.load = function (name, onLoadedCallback) {
         this.onLoadedCallback = onLoadedCallback;
-        var configUrl = 'widgets/' + name + '/widget.json';
+        this.configUrl = 'widgets/' + name + '/widget.json';
 
-        if (WidgetLoader.cache.contains(configUrl)) {
+        if (WidgetLoader.cache.contains(this.configUrl)) {
             log('Widget ' + name + ' already in cache');
-            var widget = WidgetLoader.cache.get(configUrl);
+            var widget = WidgetLoader.cache.get(this.configUrl);
             onLoadedCallback(widget);
-        } else if (WidgetLoader.loading.contains(configUrl)) {
+        } else if (WidgetLoader.loading.contains(this.configUrl)) {
             log('Widget ' + name + ' is loading');
-            var loadingWidget = WidgetLoader.loading.get(configUrl);
+            var loadingWidget = WidgetLoader.loading.get(this.configUrl);
             loadingWidget.addOnceListener('loaded', onLoadedCallback);
         } else {
             log('Widget ' + name + ' is new');
             this.widget = new Widget(name);
-            WidgetLoader.loading.add(configUrl, this.widget);
-            this.loadJson(configUrl, function (config) {
+            WidgetLoader.loading.add(this.configUrl, this.widget);
+            this.loadJson(this.configUrl, function (config) {
                 this.config = config;
-                var widget = WidgetLoader.loading.get(configUrl);
+                var widget = WidgetLoader.loading.get(this.configUrl);
                 if (widget !== this.widget) {
                     log('Error, something is wrong. The widget from the loading cache does not match the widget created in the class.');
                     return;
                 }
-                WidgetLoader.cache.add(configUrl, widget);
-                WidgetLoader.loading.remove(configUrl);
 
                 // load dependencies (elements)
                 if (config.hasOwnProperty('elements')) {
@@ -253,6 +256,8 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
     WidgetLoader.prototype._checkIfLoadingComplete = function () {
         if (this.numberOfElements == this.numberOfElementsLoaded && this.indexHtmlLoaded) {
             log('Widget ' + this.widget.name + ' has been loaded');
+            WidgetLoader.cache.add(this.configUrl, this.widget);
+            WidgetLoader.loading.remove(this.configUrl);
             this.widget.emitEvent('loaded', this.widget);
             this.onLoadedCallback(this.widget);
         }
@@ -270,13 +275,153 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
 
 
     function LayoutLoader() {
-
+        this.layout = null;
     }
 
     Loader.setUpSubclass(LayoutLoader);
 
     LayoutLoader.prototype.load = function (name, onLoadedCallback) {
+        var url = 'layouts/' + name + '.html';
+        if (LayoutLoader.cache.contains(url)) {
+            log('Layout ' + name + ' already in cache');
+            var layout = WidgetLoader.cache.get(url);
+            onLoadedCallback(layout);
+        } else if (LayoutLoader.loading.contains(url)) {
+            log('Layout ' + name + ' is loading');
+            var loadingLayout = LayoutLoader.loading.get(url);
+            loadingLayout.addOnceListener('loaded', onLoadedCallback);
+        } else {
+            log('Layout ' + name + ' is new');
+            this.layout = new Layout(name);
+            LayoutLoader.loading.add(url, this.layout);
 
+
+            this.loadHtml(url, function (e) {
+                console.log('Loaded layout html: ' + e.target.href);
+
+                var layout = LayoutLoader.loading.get(url);
+                if (layout !== this.layout) {
+                    log('Error, something is wrong. The layout from the loading cache does not match the layout created in the class.');
+                    return;
+                }
+                LayoutLoader.cache.add(url, layout);
+                LayoutLoader.loading.remove(url);
+
+                this.layout.document = e.target.import;
+                onLoadedCallback(this.layout);
+                this.layout.emitEvent('loaded', this.layout);
+            }.bind(this));
+        }
+    };
+
+    function WorkspaceConfigLoader() {
+        this.workspaceConfig = {};
+        this.layoutLoaded = false;
+        this.numberOfWidgets = null;
+        this.numberOfWidgetsLoaded = 0;
+        this.onLoadedCallback = function () {
+        };
+    }
+
+    Loader.setUpSubclass(WorkspaceConfigLoader);
+
+    WorkspaceConfigLoader.prototype.load = function (name, onLoadedCallback) {
+        this.onLoadedCallback = onLoadedCallback;
+        var configUrl = 'workspace-configs/' + name + '.json';
+
+        if (WorkspaceConfigLoader.cache.contains(configUrl)) {
+            log('WorkspaceConfig ' + name + ' already in cache');
+            var workspaceConfig = WorkspaceConfigLoader.cache.get(configUrl);
+            onLoadedCallback(workspaceConfig);
+        } else if (WorkspaceConfigLoader.loading.contains(configUrl)) {
+            log('WorkspaceConfig ' + name + ' is loading');
+            var loadingWorkspaceConfig = WorkspaceConfigLoader.loading.get(configUrl);
+            loadingWorkspaceConfig.addOnceListener('loaded', onLoadedCallback);
+        } else {
+            log('WorkspaceConfig ' + name + ' is new');
+            this.workspaceConfig = new WorkspaceConfig(name);
+            WorkspaceConfigLoader.loading.add(configUrl, this.workspaceConfig);
+            this.loadJson(configUrl, function (config) {
+                this.config = config;
+                var workspaceConfig = WorkspaceConfigLoader.loading.get(configUrl);
+                if (workspaceConfig !== this.workspaceConfig) {
+                    log('Error, something is wrong. The config from the loading cache does not match the config created in the class.');
+                    return;
+                }
+
+                // load layout
+                if (!this.config.hasOwnProperty('layout')) {
+                    log('Loading failed: WorkspaceConfig has no layout!');
+                    return;
+                }
+                var layoutLoader = new LayoutLoader();
+                layoutLoader.load(this.config.layout, function (layout) {
+                    this.layoutLoaded = true;
+                    this.workspaceConfig.layout = layout;
+                    this._checkIfLoadingComplete();
+                }.bind(this));
+
+
+                // load widgets
+                this._loadWidgets();
+
+
+            }.bind(this));
+        }
+    };
+
+    WorkspaceConfigLoader.prototype._loadWidgets = function () {
+        // k: widgetname, v: viewTarget
+        var widgetsToLoad = Object.create(null);
+        if (!this.config.hasOwnProperty('mainWorkspace') || !this.config.mainWorkspace) {
+            log(this.config.name + ' does not have a main workspace!');
+            return;
+        }
+        widgetsToLoad[this.config.mainWorkspace] = 'mainWorkspace';
+
+        if (this.config.hasOwnProperty('sideWorkspaceLeft') && this.config.sideWorkspaceLeft) {
+            log(this.config.name + ' has a side workspace left');
+            widgetsToLoad[this.config.sideWorkspaceLeft] = 'sideWorkspaceLeft';
+        }
+
+        if (this.config.hasOwnProperty('sideWorkspaceRight') && this.config.sideWorkspaceRight) {
+            log(this.config.name + ' has a side workspace right');
+            widgetsToLoad[this.config.sideWorkspaceRight] = 'sideWorkspaceRight';
+        }
+
+
+        if (this.config.hasOwnProperty('widgets') && Object.keys(this.config.widgets).length > 0) {
+            log(this.config.name + ' has free widgets');
+            var freeWidgets = 0;
+            for (var widgetName in this.config.widgets) {
+                if (this.config.widgets.hasOwnProperty(widgetName)) {
+                    widgetsToLoad[widgetName] = 'widget' + (freeWidgets++);
+                }
+            }
+        }
+        this.numberOfWidgets = Object.keys(widgetsToLoad).length;
+        log(this.config.name + ' has ' + this.numberOfWidgets + ' widgets');
+        for (var widgetName in widgetsToLoad) {
+            var widgetLoader = new WidgetLoader();
+            widgetLoader.load(widgetName, function (widget) {
+                widget.viewTarget = widgetsToLoad[widgetName];
+                this.workspaceConfig.widgets.push(widget);
+                this.numberOfWidgetsLoaded += 1;
+                this._checkIfLoadingComplete();
+            }.bind(this));
+        }
+
+    };
+
+
+    WorkspaceConfigLoader.prototype._checkIfLoadingComplete = function () {
+        if (this.layoutLoaded && this.numberOfWidgets == this.numberOfWidgetsLoaded) {
+            log('WorkspaceConfig ' + this.workspaceConfig.name + ' has been loaded');
+            WorkspaceConfigLoader.cache.add(this.configUrl, this.widget);
+            WorkspaceConfigLoader.loading.remove(this.configUrl);
+            this.workspaceConfig.emitEvent('loaded', this.workspaceConfig);
+            this.onLoadedCallback(this.workspaceConfig);
+        }
     };
 
 
@@ -286,11 +431,23 @@ define('core-loader', ['heir', 'eventEmitter'], function (heir, EventEmitter) {
              BEGIN TESTS
              */
 
-            var wl = new WidgetLoader();
-            wl.load('sprintplanning2', function (widget) {
-                log('## widget has been loaded');
-                console.dir(widget);
+            var wcl = new WorkspaceConfigLoader();
+            wcl.load('sprintplanning2', function (wConfig) {
+                console.log('%%% loaded workspaceConfig');
+                console.dir(wConfig);
             });
+
+            //var wl = new WidgetLoader();
+            //wl.load('sprintplanning2', function (widget) {
+            //    log('## widget has been loaded');
+            //    console.dir(widget);
+            //});
+            //
+            //var ll = new LayoutLoader();
+            //ll.load('workspace', function (layout) {
+            //    log('## layout loaded');
+            //    console.dir(layout);
+            //});
 
             /*
              END TESTS
