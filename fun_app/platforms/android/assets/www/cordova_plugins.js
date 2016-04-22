bcb2a37fe2835756e7679080d42e33d3fd4de96b
Florian <flofank@gmx.ch>
2016-04-22 10:57:21 +0200

    no message

--- fun_app/platforms/android/assets/www/cordova_plugins.js
+++ fun_app/platforms/android/assets/www/cordova_plugins.js
@@ -1,16 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/org.jshybugger.cordova/www/jsHybuggerLoader.js",
-        "id": "org.jshybugger.cordova.jsHybuggerLoader",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
         "clobbers": [
@@ -37,14 +27,24 @@ module.exports = [
         "clobbers": [
             "CameraPopoverHandle"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.jshybugger.cordova/www/jsHybuggerLoader.js",
+        "id": "org.jshybugger.cordova.jsHybuggerLoader",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-camera": "2.1.1",
     "cordova-plugin-whitelist": "1.2.1",
-    "org.jshybugger.cordova": "4.5.9",
-    "cordova-plugin-camera": "2.1.1"
+    "org.jshybugger.cordova": "4.5.9"
 };
 // BOTTOM OF METADATA
 });
