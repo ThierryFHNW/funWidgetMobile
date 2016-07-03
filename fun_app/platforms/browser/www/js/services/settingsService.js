ff4e344815c99a6f8e7901cb2e24cad56da0a701
Florian Fankhauser <fankiflo@gmail.com>
2016-07-03 20:08:08 +0200

    Added user feedback to app

---
+++ fun_app/platforms/browser/www/js/services/settingsService.js
@@ -0,0 +1,52 @@
+
+$app.service('settingsService', function() {
+    var settings;
+
+    this.getConfigurations = function() {
+        return settings.configurations;
+    };
+
+    this.getDefaultConfiguration = function() {
+        return settings.default;
+    };
+
+    this.setDefaultConfiguration = function(id) {
+        settings.default = id;
+        saveSettings();
+    };
+
+    this.add = function(configuration) {
+        configuration.id = new Date().getTime();
+        settings.configurations[configuration.id] = configuration;
+        saveSettings();
+    };
+
+    this.edit = function(configuration) {
+        var conf = settings.configurations[configuration.id];
+        conf.server = configuration.server;
+        conf.project = configuration.project;
+        conf.user = configuration.user;
+        conf.password = configuration.password;
+        saveSettings();
+    };
+
+    this.remove = function(configuration) {
+        delete settings.configurations[configuration.id];
+        saveSettings();
+    };
+
+    var saveSettings = function() {
+        window.localStorage.setItem("settings", JSON.stringify(settings));
+    };
+
+    var loadSettings = function() {
+        settings = JSON.parse(window.localStorage.getItem("settings"));
+        if (!settings) {
+            settings = {
+                configurations: {}
+            };
+        }
+    };
+
+    loadSettings();
+});
